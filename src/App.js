87c605e9214b73bf652739aef18b7dfbc2a60a1b
emmanuel <ogahemmanuel75@gmail.com>
2022-08-25 13:11:29 +0000

    Change background color onclick

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import './App.css';
 const List = ({ items }) => {
   const [isActive, setIsActive] = useState(false);
   
-  const handleChange = e => {
-    e.preventDefault();
-    console.log(e.target.value);
+  const handleChange = () => {
+    setIsActive(!isActive)
   }
   return (
   <Fragment>
       <ul className="List">
         {items.map(item => (
-          <li key={item.name} onClick={handleChange} className={`List__item List__item--${item.color}`}>
+          <li key={item.name} onClick={ handleChange} className={` ${(isActive === true) ? 'tk': `List__item List__item--${item.color}`}`}>
             {item.name}
           </li>
         ))}
