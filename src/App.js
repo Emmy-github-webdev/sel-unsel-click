12d5f90e9ccc087d4c6c00d6cab513a6972a7c53
emmanuel <ogahemmanuel75@gmail.com>
2022-08-25 10:35:07 +0000

    Update css to change cursor to pointer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 const List = ({ items }) => {
   const [isActive, setIsActive] = useState(false);
   
-  
+  const handleChange = e => {
+    e.preventDefault();
+    console.log(e.target.value);
+  }
   return (
   <Fragment>
       <ul className="List">
         {items.map(item => (
-          <li key={item.name} className={`List__item List__item--${item.color}`}>
+          <li key={item.name} onClick={handleChange} className={`List__item List__item--${item.color}`}>
             {item.name}
           </li>
         ))}
