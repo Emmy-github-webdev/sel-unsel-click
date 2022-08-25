830d7cee3e21381351c86db504617d611b5be095
emmanuel <ogahemmanuel75@gmail.com>
2022-08-25 15:43:10 +0000

    Refactor the change handler

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 const List = ({ items }) => {
   const [isActive, setIsActive] = useState(false);
   
-  const handleChange = () => {
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log('Target', e.target.tag)
     setIsActive(!isActive)
   }
   return (
   <Fragment>
       <ul className="List">
         {items.map(item => (
-          <li key={item.name} onClick={ handleChange} className={` ${(isActive === true) ? 'tk': `List__item List__item--${item.color}`}`}>
+          <li key={item.name} onClick={ handleChange} className={` ${(isActive === true) ? 'List__item tk': `List__item List__item--${item.color}`}`}>
             {item.name}
           </li>
         ))}
