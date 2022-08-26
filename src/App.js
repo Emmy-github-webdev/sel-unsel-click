011bb08bdcf0d077f726609b40124506427ee6ca
emmanuel <ogahemmanuel75@gmail.com>
2022-08-26 13:20:58 +0000

    Add key props

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import './App.css';
 const List = ({ items }) => {
   const [isActive, setIsActive] = useState(false);
   
-  const handleChange = (e) => {
-    e.preventDefault();
-    console.log('Target', e.target.tag)
+  const handleChange = () => {
     setIsActive(!isActive)
   }
   return (
   <Fragment>
       <ul className="List">
-        {items.map(item => (
-          <li key={item.name} onClick={ handleChange} className={` ${(isActive === true) ? 'List__item tk': `List__item List__item--${item.color}`}`}>
+        {items.map((item, index) => (
+          <li key={index} onClick={ handleChange} className={` ${(isActive === true) ? 'List__item tk': `List__item List__item--${item.color}`}`}>
             {item.name}
           </li>
         ))}
